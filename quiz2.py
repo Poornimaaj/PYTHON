d9604386d0bb83fbaec709df8c19fcdb37884781
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2017-12-26 22:41:06 +0530

    Create quiz2.py

---
+++ quiz2.py
@@ -0,0 +1,10 @@
+language=['python']
+languages=['python','c','c++','java','perl']
+print(language)
+print(languages)
+print(languages[0:3])
+print(languages[1:4])
+print(languages[2])
+print(languages[5])
+print(languages[0] + " and " + languages[1] + " are quite different!")
+print("accessing the last element of the list: " + languages[-1])
