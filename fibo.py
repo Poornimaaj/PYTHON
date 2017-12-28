620e25f8627cf1ba0132121d8d17eed6432ec64b
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2017-12-28 12:47:09 +0530

    Update fibo.py

--- fibo.py
+++ fibo.py
@@ -1,10 +1,10 @@
 def fib2(n):
-    result=[]
-    a, b=0, # -*- coding: latin-1 -*-
-    while b < n:
-        result.append(b)
-        a,b=b, a+b
-    return result
-f100 = fib2(100)
-print f100
-    
+     result = []
+     a, b = 0, 1
+     while b < n:
+          result.append(b)   
+          a, b = b, a+b
+     return result
+ 
+f100 = fib2(100)    
+print (f100)    
