55b17248fa40fc918d9cfcb9ad15b9d8ac9db95b
Poornimaaj <34641743+Poornimaaj@users.noreply.github.com>
2017-12-28 12:38:03 +0530

    Create 29.py

---
+++ 29.py
@@ -0,0 +1,7 @@
+courses=('python programming','rdms','web technology','software engineering')
+electives=('buisness intelligence','data analytics')
+len(courses)
+print(len(courses))
+print(courses)
+merge_list=courses + electives
+print(merge_list)
